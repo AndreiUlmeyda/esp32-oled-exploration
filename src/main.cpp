a988c47bd5afd5a71ebfe28344a45972220ab7e3
Adrian Schurz <adrian.schurz@check24.de>
2021-03-18 21:15:45 +0100

    Reduce to the relevant examples

--- src/main.cpp
+++ src/main.cpp
@@ -94,11 +94,8 @@ SAppMenu menu;
 
 const char *menuItems[] =
     {
-        "draw bitmap",
-        "sprites",
-        "fonts",
-        "nano canvas",
         "draw lines",
+        "fonts",
 };
 
 void waitForSerialConnection()
@@ -132,65 +129,7 @@ void setup()
   ssd1306_showMenu8(&menu);
 }
 
-uint8_t rotation = 0;
-
-static void bitmapDemo()
-{
-  ssd1306_setColor(RGB_COLOR8(64, 64, 255));
-  if (rotation & 1)
-    ssd1306_drawMonoBitmap8(0, 0, 64, 64, Owl_64x64);
-  else
-    ssd1306_drawMonoBitmap8(0, 0, 96, 64, Owl);
-  ssd1306_drawBitmap8(0, 0, 8, 8, heartImage8);
-  ssd1306_setColor(RGB_COLOR8(255, 64, 64));
-  ssd1306_drawMonoBitmap8(0, 16, 8, 8, heartImage);
-  delay(3000);
-}
-
-/* Sprites are not implemented for color modes.
- * But there is NanoEngine support
- * To make example clear, we use lambda as function pointer. Since lambda can be
- * passed to function only if it doesn't capture, all variables should be global.
- * Refer to C++ documentation.
- */
-NanoPoint sprite;
 NanoEngine8 engine;
-static void spriteDemo()
-{
-  // We not need to clear screen, engine will do it for us
-  engine.begin();
-  // Force engine to refresh the screen
-  engine.refresh();
-  // Set function to draw our sprite
-  engine.drawCallback([]() -> bool {
-    engine.canvas.clear();
-    engine.canvas.setColor(RGB_COLOR8(255, 32, 32));
-    engine.canvas.drawBitmap1(sprite.x, sprite.y, 8, 8, heartImage);
-    return true;
-  });
-  sprite.x = 0;
-  sprite.y = 0;
-  for (int i = 0; i < 250; i++)
-  {
-    delay(15);
-    // Tell the engine to refresh screen at old sprite position
-    engine.refresh(sprite.x, sprite.y, sprite.x + 8 - 1, sprite.y + 8 - 1);
-    sprite.x++;
-    if (sprite.x >= ssd1306_displayWidth())
-    {
-      sprite.x = 0;
-    }
-    sprite.y++;
-    if (sprite.y >= ssd1306_displayHeight())
-    {
-      sprite.y = 0;
-    }
-    // Tell the engine to refresh screen at new sprite position
-    engine.refresh(sprite.x, sprite.y, sprite.x + 8 - 1, sprite.y + 8 - 1);
-    // Do refresh required parts of screen
-    engine.display();
-  }
-}
 
 static void textDemo()
 {
@@ -212,25 +151,6 @@ static void textDemo()
   delay(3000);
 }
 
-static void canvasDemo()
-{
-  uint8_t buffer[64 * 16 / 8];
-  NanoCanvas1_8 canvas(64, 16, buffer);
-  ssd1306_setColor(RGB_COLOR8(0, 255, 0));
-  ssd1306_setFixedFont(ssd1306xled_font6x8);
-  ssd1306_clearScreen8();
-  canvas.clear();
-  canvas.fillRect(10, 3, 80, 5);
-  canvas.blt((ssd1306_displayWidth() - 64) / 2, 1);
-  delay(500);
-  canvas.fillRect(50, 1, 60, 15);
-  canvas.blt((ssd1306_displayWidth() - 64) / 2, 1);
-  delay(1500);
-  canvas.printFixed(20, 1, " DEMO ", STYLE_BOLD);
-  canvas.blt((ssd1306_displayWidth() - 64) / 2, 1);
-  delay(3000);
-}
-
 static void drawLinesDemo()
 {
   /* SSD1331 controller has hardware acceleration, thus                *
@@ -249,37 +169,20 @@ static void drawLinesDemo()
 
 void loop()
 {
-  Serial.println("loop");
   delay(1000);
   switch (ssd1306_menuSelection(&menu))
   {
   case 0:
-    bitmapDemo();
+    drawLinesDemo();
     break;
 
   case 1:
-    spriteDemo();
-    break;
-
-  case 2:
     textDemo();
-    break;
-
-  case 3:
-    canvasDemo();
-    break;
-
-  case 4:
-    drawLinesDemo();
     break;
 
   default:
     break;
   }
-  // if ((menu.count - 1) == ssd1306_menuSelection(&menu))
-  // {
-  //   ssd1331_setRotation((++rotation) & 0x03);
-  // }
   ssd1306_clearScreen8();
   ssd1306_setColor(RGB_COLOR8(255, 255, 255));
   ssd1306_showMenu8(&menu);
