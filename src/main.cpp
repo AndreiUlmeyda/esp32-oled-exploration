f7f0d383ad91968b0a7f8b17375b0588fd25008b
Adrian Schurz <adrian.schurz@check24.de>
2021-03-19 20:00:18 +0100

    Prototype with current values and a co2 diagram

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ SCD30 airSensor;
 void calibrateSensor()
 {
   // measurement interval in seconds
-  airSensor.setMeasurementInterval(2);
+  airSensor.setMeasurementInterval(1);
   // approx. altitude of Dresden, Saxony in meters
   airSensor.setAltitudeCompensation(112);
   // approx. avg. pressure in Dresden, Saxony in mBar
@@ -49,6 +49,8 @@ void setupI2C()
   Wire.begin();
 }
 
+int history[96] = {};
+
 void setup()
 {
   startSerialConnection();
@@ -64,27 +66,87 @@ void setup()
   // RGB functions do not work in default SSD1306 compatible mode
   ssd1306_setMode(LCD_MODE_NORMAL);
   ssd1306_clearScreen8();
+
+  for (int i = 0; i < 96; i++)
+  {
+    history[i] = 400;
+  }
 }
 
 int co2 = 0;
 float temperature = 0;
 
+void shiftHistory()
+{
+  for (int i = 0; i < 95; i++)
+  {
+    history[i] = history[i + 1];
+  }
+}
+
 void loop()
 {
+  Serial.println("derp");
   if (airSensor.dataAvailable())
   {
     co2 = airSensor.getCO2();
+    history[95] = co2;
     temperature = airSensor.getTemperature();
+
+    ssd1306_clearScreen8();
+    ssd1306_setFixedFont(ssd1306xled_font8x16);
+
+    String co2asString = String(co2);
+    String temperatureString = String(temperature);
+
+    char co2Text[5];
+    co2asString.toCharArray(co2Text, 5);
+    char temperatureText[6];
+    temperatureString.toCharArray(temperatureText, 6);
+    int textX = 0;
+    if (co2 >= 10000)
+    {
+      textX = 4;
+    }
+    else if (co2 >= 1000 && co2 < 10000)
+    {
+      textX = 12;
+    }
+    else
+    {
+      textX = 20;
+    }
+
+    ssd1306_setColor(RGB_COLOR8(0, 255, 0));
+    if (co2 >= 900)
+    {
+      ssd1306_setColor(RGB_COLOR8(255, 255, 0));
+    }
+    if (co2 >= 1500)
+    {
+      ssd1306_setColor(RGB_COLOR8(255, 0, 0));
+    }
+
+    ssd1306_printFixed8(textX, 48, co2Text, STYLE_BOLD);
+
+    ssd1306_setColor(RGB_COLOR8(255, 255, 255));
+    ssd1306_printFixed8(50, 48, temperatureText, STYLE_BOLD);
+
+    for (int i = 0; i < 96; i++)
+    {
+      int y = 47 - min(47, ((history[i] - 400) / 20));
+      ssd1331_drawLine(i, 47, i, y, RGB_COLOR8(0, 255, 0));
+      if (history[i] >= 900)
+      {
+        // ssd1331_drawLine(i, 22, i, y, RGB_COLOR8(0, 255, 255)); // seems to hit the right pixel
+        ssd1331_drawLine(i, 47, i, y, RGB_COLOR8(0, 255, 255));
+      }
+
+      if (history[i] >= 1500)
+      {
+        ssd1331_drawLine(i, 47, i, y, RGB_COLOR8(0, 0, 255));
+      }
+    }
+    shiftHistory();
   }
-
-  delay(1000);
-  String text = String(co2) + " " + String(temperature);
-  ssd1306_clearScreen8();
-  ssd1306_setFixedFont(ssd1306xled_font8x16);
-  ssd1306_setColor(RGB_COLOR8(102, 255, 51));
-  char text2[12];
-  text.toCharArray(text2, 12);
-  ssd1306_printFixed8(4, 48, text2, STYLE_BOLD);
-
-  ssd1331_drawLine(0, 0, 15, 15, RGB_COLOR8(0, 0, 255));
 }
